40d3a3d69dc4f443e392e84e88666eed05c0b4f3
Boyan Bonev <bobo.bonev@gmail.com>
2015-08-30 01:01:42 +0300

    Update predict.m
    The predict function wasn't implemented. The proposed implementation finds where the hypothesis gives a result bigger than 0.5 and presets the result vector with 1 for the given position. Tested and it works - I submitted the assignment successfully.

--- mlclass-ex2/predict.m
+++ mlclass-ex2/predict.m
@@ -15,11 +15,11 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
+ h = sigmoid(X * theta);
+ onesPos = find(h >= 0.5);
+ for i=1:length(onesPos);
+    p(onesPos(i)) = 1;
+ end;
 
 % =========================================================================
 
